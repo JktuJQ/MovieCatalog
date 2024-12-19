dfdddfdac5aaf6ec82cee6263ae05d757952b123
JktuJQ <odmamontov@gmail.com>
2024-12-19 14:45:02 +0300

    Re-created models

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from polls.models import Poll, Choice, Question
 
 
-admin.site.register(Poll)
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Poll)
